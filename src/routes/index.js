69750a174ce9596a6bf039cd5ecf375ae8b290d1
YJSIT_03 <YJSIT_03>
2017-02-24 09:35:35 +0800

    change Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,10 @@
-/**
- * Created by vimplus on 2017/02/22.
- */
 
 import React from "react";
-import ReactDOM from "react-dom";
 import { Router, Route, IndexRoute, Link, IndexLink, browserHistory, hashHistory} from 'react-router';
 
-import Index from "../routes/HelloWorld";
-import List from "../routes/BlogList";
-import About from "../routes/About";
-
-import routes from "../routes";
+import Index from "./HelloWorld";
+import List from "./BlogList";
+import About from "./About";
 
 class App extends React.Component {
     render() {
@@ -26,7 +20,13 @@ class App extends React.Component {
     }
 }
 
-ReactDOM.render(
-    <Router history = {browserHistory} routes = {routes}/>,
-    document.getElementById("APP")
-);
+const routes = [
+    { path: 'list', component: List },
+    { path: 'about', component: About }
+]
+
+module.exports = {
+    path: '/',
+    component: App,
+    childRoutes: routes
+}
