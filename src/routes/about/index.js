d31ef0550b0f7a08404f6166236f42fd75f16639
JtxBoy <tx@vimplus.com>
2017-03-20 11:29:54 +0800

    修改About为about

---
+++ src/routes/about/index.js
@@ -0,0 +1,19 @@
+/**
+ * @overview: About Page
+ * @author: txBoy
+ * @created: 2017-02-23.
+ */
+import React from "react";
+import css from "css/master.scss";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                关于乐潇游
+                乐潇游 - 快乐，潇洒，自游儿~~~
+            </div>
+        );
+    }
+}
+export default About;
